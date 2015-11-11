1c7e7cd79e6884e2e509a1cbfa2fdb4b6d82348a
eric mcgregor <eric.mcgregor@gmail.com>
2015-11-11 09:04:15 -0600

    github repo

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'ericmcgregor:bootstrap4scss',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'meteor implementation of bootstrap 4 scss',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ericmcgregor/bootstrap4scss.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -25,8 +25,10 @@ Package.onUse(function(api) {
     'ecmascript',
   	'fourseven:scss'
   ]);
+  //path to bootstrap
   var bootstrapPath = path.join(path.resolve("."), "packages", "bootstrap4scss", "bootstrap");
 
+  //files in the mixin directory
   var mixinSrc = path.join(bootstrapPath, "scss/mixins")
   var mixinSrcFiles = fs.readdirSync(mixinSrc);
 
@@ -35,7 +37,6 @@ Package.onUse(function(api) {
     addPath = path.join('bootstrap/scss/mixins', file);
     api.add_files(addPath, "client", {isImport:true});
   });
-
 
   var scssSrc = path.join(bootstrapPath, "scss")
   var scssSrcFiles = fs.readdirSync(scssSrc);
